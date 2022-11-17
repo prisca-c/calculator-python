2b0f60b2dcecc69a1b1b1e09b2fc40dd9dc1d26c
Prisca <prisca.castanier@gmail.com>
2022-11-17 22:48:22 +0100

    slimplified button's method

--- components/button.py
+++ components/button.py
@@ -4,14 +4,15 @@ from tkinter import Button
 class ButtonSingle:
 
     #  Create a reusable button
-    def __init__(self, value, button_width, row, column, handle_click):
+    def __init__(self, value, row, column, handle_click):
         btn = Button(text=value, command=lambda: handle_click(value))
         if value == "=" or value == "/" or value == "*" or value == "-" or value == "+":
             btn.configure(bg="orange")
-        btn.configure(width=button_width, height=2)
-        btn.columnconfigure(column, weight=1)
+
         if value == "0":
+            btn.configure(width=13, height=2)
             btn.grid(row=row, column=column, columnspan=2)
         else:
+            btn.configure(width=6, height=2)
             btn.grid(row=row, column=column)
 
