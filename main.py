05c285b6cc7ae1e927ed6a862e88fb59d24bb393
Prisca <prisca.castanier@gmail.com>
2022-11-17 18:38:06 +0100

    displaying buttons and screen in the main component & added method that handle button's click

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-import tkinter as tk
+from tkinter import Tk, Label, Button, IntVar, ttk
+from components.button_pannel import ButtonPanel
+from components.screen import Screen
+from components.logic import Logic
 
-window = tk.Tk()  # Create a window
 
+# Create method that handle value from buttons
+def handle_click(value):
+    print(value)
+
+
+class Calculator:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
+        master.geometry("700x800")
+
+        self.total = 0
+        self.raw_first_number = ""
+        self.raw_second_number = ""
+        self.int_first_number = 0
+        self.int_second_number = 0
+        self.operator = ""
+
+        self.label_total = IntVar()
+        self.label_total.set(self.total)
+        self.total_label = Label(master, textvariable=self.label_total)
+
+        self.button_panel = ButtonPanel(master, handle_click)  # Create the button's panel
+        self.screen = Screen(master, self.total)  # Create the screen
+
+
+root = Tk()
+im_beginner = Calculator(root)
+root.mainloop()
