cedcad560a9adc12715a3c02ef4432fe0c8c5b69
Prisca <prisca.castanier@gmail.com>
2022-11-20 01:45:23 +0100

    removed unused variables in button_pannel.py

--- components/button_pannel.py
+++ components/button_pannel.py
@@ -11,13 +11,6 @@ class ButtonPanel:
         def handle_click(btn_value):
             Logic(btn_value)
 
-        self.total = Logic.total
-        self.raw_first_number = ""
-        self.raw_second_number = ""
-        self.int_first_number = 0
-        self.int_second_number = 0
-        self.operator = ""
-
         #  Create each buttons
         ButtonSingle("+", 1, 3, handle_click)
         ButtonSingle("-", 2, 3, handle_click)
